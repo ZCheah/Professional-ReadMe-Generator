6bc9c4661323ea1e75d74508338dc0e239bd19d9
ZCheah <zcheah98@gmail.com>
2024-01-23 19:36:51 +0000

    complete file

---
+++ starter/index.js
@@ -0,0 +1,77 @@
+// Variables required for the program
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// An array of questions for users
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+
+    {
+        type: "input",
+        name: "title",
+        message: "What is your projects name?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:"
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GLP3.0", "BSD2", "BSD3", "None"]
+    },
+
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+
+    {
+        type: "input",
+        name: "contributors",
+        message: "What does the user need to know about contributing to the repo?"
+    }
+];
+
+// Writes README file
+function writeToFile(fileName, data) {
+}
+
+// Initialize program
+function init() {
+
+}
+
+// Call function
+init();
